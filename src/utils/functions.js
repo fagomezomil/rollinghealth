334c9a8e9758290ba21b21a17a06975ca0747907
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2024-08-25 23:09:18 -0300

    48hs de anticipacion para cancelar turnos y loading boton login funcionando

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,3 +2,13 @@ export function irAlTop() {
     window.scrollTo(0, 0);
   }
   
+
+export const convertirFechaHora = (fecha, horario) => {
+  const [dia, mes, anio] = fecha.split('-').map(Number);
+  const [hora, minutoStr] = horario.split(':');
+  const [minuto] = minutoStr.split(' ');
+  
+  const fechaHoraTurno = new Date(anio, mes - 1, dia, hora, minuto);
+  return fechaHoraTurno;
+};
+
